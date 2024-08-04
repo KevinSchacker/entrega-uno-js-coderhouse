5ab8904d75054c54be02a353a7e68316cd3057ac
Kevin Schacker <kevinschacker11@gmail.com>
2024-08-03 22:35:06 -0300

    cambios en js

--- js/script.js
+++ js/script.js
@@ -1,10 +1,10 @@
 // Función para registrar un usuario
 function registerUser(event) {
     event.preventDefault(); 
-  
+
     const username = document.getElementById("registerUsername").value;
     const password = document.getElementById("registerPassword").value;
-  
+
     if (localStorage.getItem(username)) {
         alert("Este usuario ya se encuentra registrado.");
     } else {
@@ -14,7 +14,7 @@ function registerUser(event) {
     }
 }
 
-/// Función para iniciar sesión
+// Función para iniciar sesión
 function loginUser(event) {
     event.preventDefault(); 
 
@@ -24,14 +24,15 @@ function loginUser(event) {
     const storedPassword = localStorage.getItem(username);
 
     if (storedPassword && storedPassword === password) {
-        alert("BIENVENIDO " + username);  // Mostrar bienvenida primero
-        window.location.href = "/pags/productos.html"; // Luego redirigir
+        alert("Inicio de sesión exitoso.");
+        window.location.href = "/pags/productos.html";
+        alert("BIENVENIDO " + username);
     } else {
         alert("VERIFIQUE: Usuario o contraseña incorrectos.");
     }
 }
-
-// Asignar eventos a los formularios
-document.getElementById("registerForm").addEventListener("submit", registerUser);
-document.getElementById("loginForm").addEventListener("submit", loginUser);
-
+    
+    // Asignar eventos a los formularios
+    document.getElementById("registerForm").addEventListener("submit", registerUser);
+    document.getElementById("loginForm").addEventListener("submit", loginUser);
+;
